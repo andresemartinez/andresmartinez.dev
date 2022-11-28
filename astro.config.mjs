5017f7802cb253fd6550ea1e12da49dd6221794a
Andres Martinez <andres93.lml@gmail.com>
2022-11-28 11:40:59 -0300

    Updating shiki theme to Dark Purple

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import darkPurpleTheme from "./dark-purple-theme.json" assert { type: "json" };
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://andresmartinez.dev",
+  markdown: {
+    shikiConfig: {
+      theme: darkPurpleTheme,
+      wrap: true,
+    },
+  },
   integrations: [mdx(), sitemap(), tailwind()],
 });
