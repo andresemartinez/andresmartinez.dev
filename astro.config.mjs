c831fe593bccec9a5fbbcf7a52244ac891da7c83
Andres Martinez <andres93.lml@gmail.com>
2022-11-28 12:37:57 -0300

    Improving links that take you out of the page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,46 @@
 import { defineConfig } from "astro/config";
-
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeSlug from "rehype-slug";
+import remarkCodeTitles from "remark-code-titles";
 import darkPurpleTheme from "./dark-purple-theme.json" assert { type: "json" };
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://andresmartinez.dev",
   markdown: {
+    remarkPlugins: [remarkCodeTitles],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noreferrer noopener"],
+          content: {
+            type: "text",
+            value: "↗",
+          },
+        },
+      ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            class: "heading-link heading-link--hidden---effects",
+            "data-heading-link": true,
+          },
+          behavior: "wrap",
+        },
+      ],
+    ],
     shikiConfig: {
       theme: darkPurpleTheme,
       wrap: true,
     },
+    extendDefaultPlugins: true,
   },
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind()],
 });
